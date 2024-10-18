253c36f35628a05287f5ddb02bcf9c0c8589f319
kazuma1989 <kazuma.ebina@quipper.com>
2024-10-19 06:15:03 +0900

    test echo labels

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-export const script = async ({ github, context }) => {
+/**
+ * @param {import("@octokit/rest").Octokit} github
+ * @param {{
+ *  repo: {
+ *    owner: string
+ *    repo: string
+ * }
+ * }} context
+ */
+export const script = async (github, context) => {
   console.log(context);
+
+  const { data } = await github.request(
+    "GET /repos/{owner}/{repo}/issues/{issue_number}",
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: 1,
+    }
+  );
+
+  return data.labels;
 };
--- index.test.js
+++ index.test.js
@@ -1,9 +1,19 @@
+import { Octokit } from "@octokit/rest";
+import process from "node:process";
 import test from "node:test";
 import { script } from "./index.js";
 
 test("", async () => {
-  script({
-    github: {},
-    context: {},
+  const github = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
   });
+
+  const labels = await script(github, {
+    repo: {
+      owner: "kazuma1989",
+      repo: "trial-octokit",
+    },
+  });
+
+  console.log(labels)
 });
